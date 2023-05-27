b3e62f11ee60ae1b89229a1b2b975ad4cb9d1a43
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-05-27 20:08:31 -0300

    avançando com arrays

---
+++ avancando_c_arrays/copiando_array.js
@@ -0,0 +1,3 @@
+const notas = [7, 7, 8, 9];
+const novasNotas = [5,...notas, 10] // clonar arrays || novasNotas.push(10)
+console.log(novasNotas)
---
+++ avancando_c_arrays/set.js
@@ -0,0 +1,3 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+const nomesAtualizados = [...new Set(nomes)];
+console.log(nomesAtualizados);
