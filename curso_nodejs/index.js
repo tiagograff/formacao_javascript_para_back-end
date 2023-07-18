793bab9915bfeae3e1df3476f718f6b5a4a0184d
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-07-18 15:38:19 -0300

    arrumando

--- curso_nodejs/index.js
+++ curso_nodejs/index.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.red('Hello world!'));
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados;
+}
 
-console.log('olá mundo');
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(extraiLinks(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
+
+//tambem tem a forma then
+
+pegaArquivo('./arquivos/texto.md');
